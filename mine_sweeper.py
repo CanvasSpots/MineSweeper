5452ec420d4f0f4f2d8118eccf05ca04afb8b245
Derek Bauer <derekbauer@MacBook-Pro.lan>
2023-09-13 19:51:50 -0600

    Added title

---
+++ mine_sweeper.py
@@ -0,0 +1,8 @@
+title = """
+M   M  I  N   N  EEEEE      /  SSSSS  W   W  EEEEE  EEEEE  PPPP   EEEEE  RRRR
+MM MM  I  NN  N  E         /   S      W   W  E      E      P   P  E      R   R
+M M M  I  N N N  EEE      /    SSSSS  W W W  EEE    EEE    PPPP   EEE    RRRR
+M   M  I  N  NN  E       /         S  WW WW  E      E      P      E      R   R
+M   M  I  N   N  EEEEE  /      SSSSS  W   W  EEEEE  EEEEE  P      EEEEE  R   R
+"""
+print(title)
